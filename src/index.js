652569384de891818f23216a90220aa1061e0b6b
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-13 00:55:03 +0300

    created components file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,6 @@
 import React from "react"
-import  *  as ReactDOMClient from "react-dom/client"
-
-class Header extends React.Component {
-  render() {
-    return (
-      <header>The Header</header>
-    )
-  }
-}
-
-class App extends React.Component {
-  helpText = "Help Text!"
-  render() {
-    return (
-    <div className="name">
-      <Header />
-      <h1>{this.helpText}</h1>
-      <input placeholder={this.helpText}
-        onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-      <p>{this.helpText === "Help Text!" ? "Yes" : "No"}</p>
-    </div>
-    )
-
-    const inputClick = () => {
-      console.log("Clicked")
-    }
-
-    const mouseOver= () => {
-      console.log("Mouse Over")
-    }
-  }
-}
+import * as ReactDOMClient from "react-dom/client"
+import App from "./App"
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
 
