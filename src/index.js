2cb99f87dbce1b0814aa42e19eb519c7a541f4be
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-13 00:18:10 +0300

    imported and created ReactDOMClient

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import  ReactDOM  from "react-dom"
+import  *  as ReactDOMClient from "react-dom/client"
 
 
 const inputClick = () => console.log("Clicked")
@@ -14,6 +14,6 @@ const elements = (<div className="name">
   <p>{helpText === "Help Text!" ? "Yes" : "No"}</p>
 </div>)
 
-const app = document.getElementById("App")
+const app = ReactDOMClient.createRoot(document.getElementById("App"))
 
-ReactDOM.render(elements, app)
+app.render(elements)
