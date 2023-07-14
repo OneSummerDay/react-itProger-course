4806ae8cc6cfa4e479cfe122b56052ee32120cac
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-14 23:43:45 +0300

    created "Help Text" state

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import logo from "./img/logo-og.png"
 
 
 class App extends React.Component {
-  helpText = "Help Text!"
+  constructor(props) {
+    super(props)
+    this.state = {
+      helpText: "Help text!",
+    }
+  }
+  
   render() {
     return (
     <div className="name">
       <Header />
-      <h1>{this.helpText}</h1>
-      <input placeholder={this.helpText}
+      <h1>{this.state.helpText}</h1>
+      <input placeholder={this.state.helpText}
         onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-      <p>{this.helpText === "Help Text!" ? "Yes" : "No"}</p>
+      <p>{this.state.helpText === "Help Text!" ? "Yes" : "No"}</p>
       <Image image={logo} />
       <img src={this.props.image} />
     </div>
     )
 
     const inputClick = () => {
+      this.helpText = "Changed"
       console.log("Clicked")
     }
 
